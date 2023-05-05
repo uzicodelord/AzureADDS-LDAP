4a43d0cb7b2c486b20c2333a3c31784590dadaa0
uzicodelord <oz211230.student@unt.edu.mk>
2023-05-05 02:54:49 +0200

    home and logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,11 @@
     <h1>
         Hey welcome user from azure active directory
     </h1>
+    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        Logout
+    </a>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+        @csrf
+    </form>
 </div>
